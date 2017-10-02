7c711be8ff5dc32e216867642327333b9b44d16b
Vpv Tima <vpv.tima@gmail.com>
2017-10-02 15:58:29 +0300

    Create functions.sql

---
+++ functions.sql
@@ -0,0 +1,44 @@
+/*
+Select, Select Distinct, 
+Where, And, Or, Not, 
+Order By, Insert Into, Null Values, 
+Update, Delete, Select Top, 
+Min, Max, Count, Avg, Sum, 
+Like, In, Between, Aliases, Joins, 
+Inner Join, Left Join, Right Join, Full Join, Self Join, 
+Union, Group By, Having, 
+Exists, Any, All, Select Into, Insert Into Select, 
+Null Functions, Comments
+*/
+
+/*
+-- STRING FUNCTIONS --
+CONCAT = Concatenates two or more expressions together
+REPLACE = Replaces all occurrences of a specified string
+SUBSTR = Extracts a substring from a string (starting at any position)
+SUBSTRING = Extracts a substring from a string (starting at any position)
+SUBSTRING_INDEX = Returns the substring of string before number of occurrences of delimiter
+*/
+
+/*
+-- MATH FUNCTIONS --
+AVG	Returns the average value of an expression
+CEIL	Returns the smallest integer value that is greater than or equal to a number
+COUNT	Returns the number of records in a select query
+MAX	Returns the maximum value of an expression
+MIN	Returns the minimum value of an expression
+RAND	Returns a random number or a random number within a range
+ROUND	Returns a number rounded to a certain number of decimal places
+SUM	Returns the summed value of an expression
+*/
+
+/*
+-- DATE FUNCTIONS --
+CURRENT_DATE	Returns the current date
+CURRENT_TIME	Returns the current time
+CURRENT_TIMESTAMP	Returns the current date and time
+DATEDIFF	Returns the difference in days between two date values
+NOW	Returns the current date and time
+TIME	Extracts the time value from a time/datetime expression
+TIMESTAMP	Converts an expression to a datetime value and if specified adds an optional time interval to the value
+*/
